04d051471a64073adb136eedf4d151b1f16af678
Jack Madden <jck13mad@gmail.com>
2020-03-25 00:09:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,10 +43,9 @@ class Song
  end
  
  def self.new_from_filename()
-   song = self.new
+    song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    @@all << song
     song
   end
   
